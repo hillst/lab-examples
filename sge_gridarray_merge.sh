e6cf07bf3ac44cace0503823a18ec57e91da2900
Steven Hill <hillst@onid.orst.edu>
2018-04-09 10:02:24 -0400

    Update sge_gridarray_merge.sh

--- sge_gridarray_merge.sh
+++ sge_gridarray_merge.sh
@@ -2,9 +2,10 @@
 #$-V
 #$-cwd
 #$-N merge
+#$-l h_vmem=4G,h_rt=00:30:00
 #$-hold_jid split
 
 BAM=${1} #first argument is the name of the bam
 BAM_BASE=`basename $BAM .bam` #extract the prefix to .bam
-samtools view ${BAM_BASE}.{1..22}.bam > ${BAM_BASE}.merged.bam #recombine the split chromosomes.
+samtools view -bh ${BAM_BASE}.{1..22}.sorted.bam > ${BAM_BASE}.sorted.bam #recombine the split chromosomes.
 
