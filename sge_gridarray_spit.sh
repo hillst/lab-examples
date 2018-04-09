555c1d3a10368048ca55a036b9198f5295ca90d4
Steven Hill <hillst@onid.orst.edu>
2018-04-09 09:49:46 -0400

    Update sge_gridarray_spit.sh

--- sge_gridarray_spit.sh
+++ sge_gridarray_spit.sh
@@ -3,8 +3,14 @@
 #$-N split
 #$-t 1-22:1
 
-BAM=${1}
-BAM_BASE=`basename $BAM .bam`
+#-t flag sets the SGE_TASK_ID to be one of 1-22, we then use that number to extract each chromosome
 
-samtools view ${BAM} ${SGE_TASK_ID} > ${BAM_BASE}.${SGE_TASK_ID}.bam
+# qsub sge_gridarray_split.sh mybam.bam
+
+BAM=${1} #takes first argument as bam
+BAM_BASE=`basename $BAM .bam` #extracts mybam from mybam.bam
+
+samtools view ${BAM} ${SGE_TASK_ID} > ${BAM_BASE}.${SGE_TASK_ID}.bam #splits by chromosome.
+
+#samtools view ${BAM} chr${SGE_TASK_ID} > ${BAM_BASE}.${SGE_TASK_ID}.bam #splits by chromosome if your tid's start with chr.
 
